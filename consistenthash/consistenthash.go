7489437cf7fba33c27cab7872aba88b6d3434eeb
leooo <281970273@qq.com>
2018-03-28 17:01:05 +0800

    一致性hash

--- consistenthash/consistenthash.go
+++ consistenthash/consistenthash.go
@@ -26,10 +26,10 @@ import (
 type Hash func(data []byte) uint32
 
 type Map struct {
-	hash     Hash
-	replicas int
-	keys     []int // Sorted
-	hashMap  map[int]string
+	hash     Hash			// hash 函数
+	replicas int			// 每个真实节点的虚拟副本个数，用于解决当节点数过少时发生数据倾斜
+	keys     []int // Sorted   有序的切片，存储所有节点
+	hashMap  map[int]string  // 存储虚拟节点与正式节点之间的对应关系
 }
 
 func New(replicas int, fn Hash) *Map {
@@ -50,18 +50,23 @@ func (m *Map) IsEmpty() bool {
 }
 
 // Adds some keys to the hash.
+// 添加节点
+//一致性哈希算法在服务节点太少时，容易因为节点分部不均匀而造成数据倾斜问题。
+//为了解决这种数据倾斜问题，一致性哈希算法引入了虚拟节点机制，即对每一个服务节点计算多个哈希，每个计算结果位置都放置一个此服务节点，称为虚拟节点。
+//replicas 即表示虚拟节点的个数（真实节点的副本数）
 func (m *Map) Add(keys ...string) {
 	for _, key := range keys {
 		for i := 0; i < m.replicas; i++ {
 			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
-			m.keys = append(m.keys, hash)
-			m.hashMap[hash] = key
+			m.keys = append(m.keys, hash)  //新增节点
+			m.hashMap[hash] = key			//存储虚拟节点对应的真节点
 		}
 	}
 	sort.Ints(m.keys)
 }
 
 // Gets the closest item in the hash to the provided key.
+// 获取key所在的节点
 func (m *Map) Get(key string) string {
 	if m.IsEmpty() {
 		return ""
@@ -69,10 +74,10 @@ func (m *Map) Get(key string) string {
 
 	hash := int(m.hash([]byte(key)))
 
-	// Binary search for appropriate replica.
+	// Binary search for appropriate replica.   二分查找法找到顺时针方向最近的一个节点
 	idx := sort.Search(len(m.keys), func(i int) bool { return m.keys[i] >= hash })
 
-	// Means we have cycled back to the first replica.
+	// Means we have cycled back to the first replica.   最后一个为第一个，成环
 	if idx == len(m.keys) {
 		idx = 0
 	}
